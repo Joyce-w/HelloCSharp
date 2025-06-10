342394a70f59e29c22ba3f3b4c364e00e7fe5586
Joyce Wong <joyce.wong@neilsonfs.com>
2025-06-10 14:54:03 -0500

    async/await practice

---
+++ HelloCSharp/Async_Await.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace Async_Await
+{
+    /*Allow task to run independently of the main flow to enable other new tasks to start
+     Async: processes bg task
+    Await pauses execution of calling method
+    Async uses Task type. Task is done in future to prevent blocking program*/
+    public class Async_Await
+    {
+        //use 'Async' keyword to mark a method as asynchronou
+        public async Task GetDataAsync()
+        {
+            try
+            {
+            var data = await GetDataFromApi();
+            return data;
+            } catch (Exception ex)
+            {
+                // Handle exceptions
+                Console.WriteLine($"An error occurred: {ex.Message}");
+
+            }
+    }
+}
